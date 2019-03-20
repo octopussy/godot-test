a8f1b7a9c0b34216510e5ab4f53b6a0e4988e97c
octopussy <uberpilot@gmail.com>
2019-03-20 16:05:42 +0300

    initial

---
+++ Player.gd
@@ -0,0 +1,48 @@
+extends Area2D
+
+export var speed = 400  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+signal hit
+
+func _ready():
+    screen_size = get_viewport_rect().size
+
+func _process(delta):
+    var velocity = Vector2()  # The player's movement vector.
+    if Input.is_action_pressed("ui_right"):
+        velocity.x += 1
+    if Input.is_action_pressed("ui_left"):
+        velocity.x -= 1
+    if Input.is_action_pressed("ui_down"):
+        velocity.y += 1
+    if Input.is_action_pressed("ui_up"):
+        velocity.y -= 1
+    if velocity.length() > 0:
+        velocity = velocity.normalized() * speed
+        $AnimatedSprite.play()
+    else:
+        $AnimatedSprite.stop()
+
+    if velocity.x != 0:
+        $AnimatedSprite.animation = "right"
+        $AnimatedSprite.flip_v = false
+        # See the note below about boolean assignment
+        $AnimatedSprite.flip_h = velocity.x < 0
+    elif velocity.y != 0:
+        $AnimatedSprite.animation = "up"
+        $AnimatedSprite.flip_v = velocity.y > 0
+
+    position += velocity * delta
+    position.x = clamp(position.x, 0, screen_size.x)
+    position.y = clamp(position.y, 0, screen_size.y)
+
+func start(pos):
+    position = pos
+    show()
+    $CollisionShape2D.disabled = false
+
+func _on_Player_body_entered(body):
+    hide()  # Player disappears after being hit.
+    emit_signal("hit")
+    $CollisionShape2D.call_deferred("set_disabled", true)
